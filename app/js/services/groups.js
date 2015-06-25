43cc98caebbe09da2784c9eced772f15efaa5fe9
Udo <writeudo@gmail.com>
2015-06-25 23:44:31 +0100

    create groups service

---
+++ app/js/services/groups.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var appServices = angular.module('pdAttendance.services', []);
+
+appCtrl.factory('LoginController', ['$firebaseArray', '$firebaseObject', 
+  function($firebaseArray, $firebaseObject){
+    return{
+
+      all: function(){}
+
+    }
+
+}]);
