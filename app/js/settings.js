11e826e29363041a3b4bf5c638e5b47eae25531a
Udo <writeudo@gmail.com>
2015-06-26 04:07:13 +0100

    created firebase refs

--- app/js/settings.js
+++ app/js/settings.js
@@ -1,1 +1,17 @@
-settings.js
+"use strict";
+var appServices = angular.module('pdAttendance.services', []);
+
+appServices.factory('settings', ['$firebaseArray', '$firebaseObject', 
+  function($firebaseArray, $firebaseObject){
+    var rootRef = new Firebase('https://pd-attendance-dev.firebaseio.com/');
+    return {
+      root: rootRef,
+      admin: rootRef.child('admin'),
+      attendance: rootRef.child('attendance'),
+      attendance_codes: rootRef.child('attendance_codes'),
+      fellows: rootRef.child('fellows'),
+      group_leaders: rootRef.child('group_leaders'),
+      test_leaders: rootRef.child('test_leaders')
+    };
+
+}]);
