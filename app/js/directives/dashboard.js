ccc0fa5393e224e790b6866b0b7a9fc55918caa6
Udo <writeudo@gmail.com>
2015-06-30 05:24:54 +0100

    created group leader directive

--- app/js/directives/dashboard.js
+++ app/js/directives/dashboard.js
@@ -40,7 +40,7 @@ directive.directive('dashboard', function(){
       $scope.showFellowsDialog = function(pdgroup){
         $scope.eachFellow = {}; //reset checkboxes
         $scope.searchFellowsList = ''; // reset search box
-         LxDialogService.open('fellows-list');
+        LxDialogService.open('fellows-list');
         $scope.currentGroup = pdgroup;
       };
 
@@ -121,10 +121,23 @@ directive.directive('pdGroup', function(){
   }
 });
 
+directive.directive('groupLeader', function(){
+  return{
+    restrict: 'E',
+    replace: true,
+    templateUrl: 'app/js/templates/group_leader.html',
+    controller: ['$rootScope', '$scope', function($rootScope, $scope){
+      $scope.fellows.$loaded().then(function(){
+        $scope.lead = $rootScope.allFellows[$scope.pdgroup.group_leader];
+      });
+    }]
+  }
+});
+
 directive.directive('avatar', function(){
   return{
     restrict: 'E',
-    replace: false,
+    replace: true,
     templateUrl: 'app/js/templates/avatar.html',
     controller: ['$rootScope', '$scope', 'Fellows', 'LxNotificationService', 'group',
     function($rootScope, $scope, Fellows, LxNotificationService, group){
@@ -135,14 +148,23 @@ directive.directive('avatar', function(){
       $scope.popup = function(person){
         var details = person.cohort.name + ' | ' + person.level;
         LxNotificationService.confirm(person.name, details, { cancel:'Remove from group', ok:'cancel' }, function(answer){
-            if(!answer){
-              var index = $scope.pdgroup.fellows.indexOf(person.$id);
-              $scope.pdgroup.fellows.splice(index, 1);
-              group.addFellows($scope.pdgroup.$id, $scope.pdgroup.fellows, function(err){
-                console.log(err);
-              });
-              Fellows.removeFromGroup(person.$id);
-            }
+          if(!answer){
+            var index = $scope.pdgroup.fellows.indexOf(person.$id);
+            $scope.pdgroup.fellows.splice(index, 1);
+            group.addFellows($scope.pdgroup.$id, $scope.pdgroup.fellows, function(err){
+              console.log(err);
+            });
+            Fellows.removeFromGroup(person.$id);
+          }
+        });
+      };
+
+      $scope.doubleClick = function(person){
+        LxNotificationService.confirm(person.name, "do you want to make this person the group leader?", { cancel:'Not now', ok:'Yes' }, function(yes){
+          if(yes){
+            group.setGroupLead($scope.pdgroup.$id, person.$id);
+            LxNotificationService.success(person.name + " is now group leader for group " + $scope.pdgroup.name);
+          }
         });
       };
 
