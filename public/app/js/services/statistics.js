9456f89609d061e8b5a0d31eabec07dd4b0a443a
Udo Nkwocha <writeudo@gmail.com>
2015-07-31 15:09:00 +0100

    created service for getting statistics

--- public/app/js/services/statistics.js
+++ public/app/js/services/statistics.js
@@ -3,6 +3,28 @@
 appServices.factory('statistics', ['$firebaseArray', '$firebaseObject', 'Refs',
 	function($firebaseArray, $firebaseObject, Refs) {
 		return{
+
+			byDate: function(date, cb){
+				var qry = Refs.attendance.child(date);
+				if(!cb){
+					return $firebaseArray(qry);
+				} else {
+					qry.on('value', function(snap){
+						cb(snap.val());
+					});
+				}
+			},
+
+			all: function(){
+				var qry = Refs.attendance;
+				if(!cb){
+					return $firebaseArray(qry);
+				} else {
+					qry.on('value', function(snap){
+						cb(snap.val());
+					});
+				}
+			}
 			
 		};
 }]);
